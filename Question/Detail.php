9974cf2d2b7c78277746738f18ecedc3a644a585
RabikClabbt <ytwk62563@gmail.com>
2024-05-01 11:17:41 +0900

    Add & edit: Question file cssファイルの作成 質問詳細にちょっと記入

---
+++ Question/Detail.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-windth,initial-scale=1.0" />
+        <title>ここには質問タイトルを置くようにする</title>
+        <link rel="stylesheet" type="text/css" href="./css/Common.css">
+    </head>
+    <body>
+        <header>
+            <p>ヘッドバーをここに置く</p>
+        </header>
+        <main>
+            <h2>レイアウトをもとに後々設計</h2>
+        </main>
+        <footer>
+            <p>フッターは協議する</p>
+        </footer>
+    </body>
+</html>
