7b1f0add87098212cc1a4bc83ae9a4e6aa4a1aff
somatakano <148741500+somatakano@users.noreply.github.com>
2024-05-08 11:58:03 +0900

    Create db-connect.php

---
+++ src/db-connect.php
@@ -0,0 +1,10 @@
+<?php
+
+    const server = ""; //サーバー名
+    const dbname = ""; //データベース名
+    const user = ""; //ユーザー名
+    const pass = ""; //パスワード
+
+    $connect = 'mysql:host='. server . ';dbname='. dbname . ';charset=utf8';
+    
+?>
