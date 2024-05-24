f97a9b7846a3549b6e2a944fd2dd534359b0be4d
RabikClabbt <ytwk62563@gmail.com>
2024-05-24 12:12:14 +0900

    Update: Header Headerファイルを.htmlから.phpに変更 userIconをデータベースからの取得に変更

--- Header/Header.php
+++ Header/Header.php
@@ -1,10 +1,10 @@
-<link rel="stylesheet" href="./css/Header.css">
+<link rel="stylesheet" href="../Header/css/Header.css">
     <div class="header">
         <div class="left">
             <div class="logo">
                 <!-- トップページへの遷移 -->
                 <a href="">
-                    <img src="./Image/IconDesign.svg" alt="サイトロゴ" width="100">
+                    <img src="../Image/IconDesign.svg" alt="サイトロゴ" width="100">
                 </a>
             </div>
         </div>
@@ -14,7 +14,13 @@
                 <a href="">
                     <div class="circle">
                         <!-- ここの画像はデータベース化取得する-->
-                        <img src="" alt="ProfileImage">
+                        <?php
+                            if(isset($_SESSION['users'])) {
+                                echo '<img src="'. $_SESSION['users']['profileIcon'] .'" alt="profileIcon">';
+                            } else {
+                                echo '<img src="../Image/DefaultIcon.svg" alt="profileIcon">';
+                            }
+                        ?>
                     </div>
                 </a>
             </div>
@@ -25,7 +31,7 @@
                 <form action="検索への遷移" method="post">
                     <input type="text" autocomplete="off" aria-autocomplete="list" aria-controls="react-autowhatever-1" class="search-input" placeholder="キーワードを入力" name="HeaderSearch" value="" spellcheck="false" data-ms-editor="true">
                     <button type="submit" class="search-button">
-                        <img src="./Image/SearchIcon.svg" width="20" height="20" alt="検索">
+                        <img src="../Image/SearchIcon.svg" width="20" height="20" alt="検索">
                     </button>
                 </form>
             </div>
