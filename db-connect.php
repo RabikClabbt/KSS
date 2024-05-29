d7d2c75e1654ebb7a66b7a8b20e892704af6f5ae
somatakano <148741500+somatakano@users.noreply.github.com>
2024-05-29 12:13:39 +0900

    ログイン画面 (#14)

---
+++ db-connect.php
@@ -0,0 +1,11 @@
+<?php
+
+
+    const server = "mysql305.phy.lolipop.lan"; //サーバー名
+    const dbname = "LAA1517467-yadix"; //データベース名
+    const user = "LAA1517467"; //ユーザー名
+    const pass = "YadiXadmin"; //パスワード
+
+    $connect = 'mysql:host='. server . ';dbname='. dbname . ';charset=utf8';
+    
+?>
