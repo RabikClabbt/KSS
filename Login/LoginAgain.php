1007fc25fdbdeac6b580eec97e330a255697e39f
somatakano <148741500+somatakano@users.noreply.github.com>
2024-05-29 12:23:39 +0900

    グループ画面作成開始

--- Login/LoginAgain.php
+++ Login/LoginAgain.php
@@ -1,32 +1,41 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ログイン画面</title>
-    <link rel="stylesheet" href="css/Login.css">
+    <link rel="stylesheet" type="text/css" href="css/Login.css">
 </head>
+
 <body>
-    <?php require '../Header/Header.html'; ?>
+    <header>
+        <!-- Header.phpを読み込む -->
+        <include src="../Header/Header.php"></include>
+    </header>
+    <main>
+        <div class="in-container">
+        <form action="LoginOut.php" method="post">
+        <h1 class="in-title">ログイン</h1>
 
-    <div class="in-container">
-    <form action="LoginOut.php" method="post">
-    <h1 class="in-title">ログイン</h1>
+        <p class="in-mailid">メールアドレス、またはユーザーID<br>
+            <input class="in-input" type="text"  placeholder="mail or ID" name="mailid">
+        </p>
 
-    <p class="in-mailid">メールアドレス、またはユーザーID<br>
-        <input class="in-input" type="text"  placeholder="mail or ID" name="mailid">
-    </p>
+        <p class="in-pass">パスワード<br>
+            <input class="in-input" type="password" placeholder="password" name="pass">
+        </p>
 
-    <p class="in-pass">パスワード<br>
-        <input class="in-input" type="password" placeholder="password" name="pass">
-    </p>
-
-    <!-- エラーメッセージ -->
-    <p class="in-error">メールアドレス・ユーザーIDまたはパスワードが違います</p>
+        <!-- エラーメッセージ -->
+        <p class="in-error">メールアドレス・ユーザーIDまたはパスワードが違います</p>
     
-    <p><a class="in-link" href="TorokuInput.php">アカウント新規作成</a></p>
+        <p><a class="in-link" href="TorokuInput.php">アカウント新規作成</a></p>
 
-    <button class="in-button" type="submit">ログイン</button>
-    </form>
-    </div>
+        <p><button class="in-button" type="submit">ログイン</button></p>
+        </form>
+        </div>
+        </main>
+    <footer>
+          
+    </footer>
 </body>
 </html>
