9688eba44a2fcaf7ba8e481677a9fcc1354c2fa1
Naachaan <2201144@s.asojuku.ac.jp>
2024-06-06 12:46:07 +0900

    トップ画面

--- Top/Globalrply.php
+++ Top/Globalrply.php
@@ -1,6 +1,10 @@
 <?php
+session_start();
 require '../db-connect.php';
 $pdo = new PDO($connect, user, pass);
+$sql = $pdo->prepare('SELECT g.commentID,g.replyID,g.commentText,g.appendFile, u.* FROM GlobalChat g JOIN Users u ON g.userID = u.userID WHERE g.commentID=?');
+$sql -> execute([$_GET['commentID']]);
+$question = $sql->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,5 +17,36 @@ $pdo = new PDO($connect, user, pass);
 <body>
     <div class="headerr">
         <?php require '../Header/Header.php'; ?>
+    </div>
+    <div class = "main-content">
+        <?php
+        foreach($question as $row){
+            ?>
+            <div class="chat-comment">
+                <div class="account">
+                    <div class="account-image">
+                        <?php
+                        if (!empty($row['profileIcon'])) {
+                            echo '<a href="../Profile/Profile.php"><img src="', htmlspecialchars($row['profileIcon']), '" alt="ProfileImage"></a>';
+                        } else {
+                            echo '<a href="../Profile/Profile.php"><img src="../image/DefaultIcon.svg" alt="ProfileImage"></a>';
+                        }
+                        ?>
+                    </div>
+                    <a href="../Profile/Profile.php?id=',$row['userID'],'"><p class="account-name"><?php htmlspecialchars($row['nickname']) ?></p></a>
+                </div>
+                <a href="Globalrply.php?id=',$row['commentID'],'" class="linkrply">
+                <p class="comment"><?php htmlspecialchars($row['commentText']) ?></p></a>';
+                <div class="rply">
+                    <img src="../Image/rplyicon.svg" alt="rply" height="20" width="20">
+                    <div class="balloon3-left">
+                        <p><?php $rplyCount ?></p>
+                    </div>
+                    <img src="../Image/goodicon.svg" alt="good" height="20" width="20">
+                </div>';
+            </div>
+            <?php
+        }
+        ?>
     </div>
 </body>
--- Top/TopPage.php
+++ Top/TopPage.php
@@ -1,29 +1,31 @@
 <?php
+
 session_start();
 require '../db-connect.php';
 $pdo = new PDO($connect, user, pass);
-$icons = [
-    ['url' => 'page1.php', 'label' => 'Q&A', 'img' => 'Image/Q&A.svg'],
-    ['url' => 'page2.php', 'label' => 'chat', 'img' => 'Image/chat.svg'],
-    ['url' => 'page3.php', 'label' => 'group-chat', 'img' => 'Image/group-chat.svg']
-];
-
+$userID = "";
+if (isset($_SESSION['users'])) {
+    $userID = $_SESSION['users'];
+} else {
+    $userID = "none"; // 例: ログインしていないユーザーのためのデフォルト値
+}
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userID = htmlspecialchars($_POST['userID']);
+    $commentText = htmlspecialchars($_POST['commentText']);
     $sql = $pdo->prepare('INSERT INTO GlobalChat (userID, commentText, appendFile) VALUES (?, ?, ?)');
+
     if (is_uploaded_file($_FILES['file']['tmp_name'])) {
         if (!file_exists('File')) {
-            if (!mkdir('File')) {
-                die('Failed to create directory.');
-            }
+            mkdir('File');
         }
         $file = 'File/' . basename($_FILES['file']['name']);
         if (move_uploaded_file($_FILES['file']['tmp_name'], $file)) {
             //ファイルが正常にアップロードされました
         } else {
-            die('Failed to move uploaded file.');
+            //アップロードされたファイルの移動に失敗
         }
     }
-    $sql->execute([$_POST['userID'], $_POST['HeaderSearch'], $file ?? null]);
+    $file=null;
 }
 ?>
 <!DOCTYPE html>
@@ -50,20 +52,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
     <div class="content">
         <div class="sideber">
-            <div class="menu-icon" onclick="goToPage('page1.php')" title="Q&A">
-                <img src="Image/Q&A.svg" alt="Q&A" class="icon-img">
+            <div class="menu-icon" onclick="goToPage('../Question/ListForum.php')" title="Q&A">
+                <img src="../image/Q&A.svg" alt="Q&A" class="icon-img">
             </div>
             <div class="menu-icon" onclick="goToPage('page2.php')" title="chat">
-                <img src="Image/chat.svg" alt="chat" class="icon-img">
+                <img src="../image/Chat.svg" alt="chat" class="icon-img">
             </div>
-            <div class="menu-icon" onclick="goToPage('page3.php')" title="group-chat">
-                <img src="Image/group-chat.svg" alt="group-chat" class="icon-img">
+            <div class="menu-icon" onclick="goToPage('../GroupControl/GroupCreate.php')" title="group-chat">
+                <img src="../image/GroupChat.svg" alt="group-chat" class="icon-img">
             </div>
         </div>
         <div class="main-content">
             <div class="global-chat">
                 <?php
-                $user = $pdo->prepare('SELECT g.*, u.* FROM GlobalChat g JOIN Users u ON g.userID = u.userID');
+                $user = $pdo->prepare('SELECT g.commentID,g.replyID,g.commentText,g.appendFile, u.* FROM GlobalChat g JOIN Users u ON g.userID = u.userID');
                 $user->execute();
                 $questions = $user->fetchAll(PDO::FETCH_ASSOC);
                 $rply = $pdo->prepare('SELECT COUNT(*) as rplyCount FROM GlobalChat WHERE replyID = ?');
@@ -71,48 +73,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     if ($row['replyID'] == null) {
                         $rply->execute([$row['commentID']]);
                         $rplya = $rply->fetch(PDO::FETCH_ASSOC);
-                        $rplyCount = $rplya['rplyCount'];
-                        echo '<div class="chat-comment">';
-                        echo '<div class="account">
-                                    <div class="account-image">
-                                    <a href="../page1" class="profile"><img src="', $row['profileIcon'], '" alt="ProfileImage"></a>
-                                    </div>
-                                    <a href="" class="profile2"><p class="account-name">', $row['nickname'], '</p></a>
-                            </div>';
-                        echo '<p class="comment">', $row['commentText'], '</p>';
-                        echo '<a href="Globalrply.php" class="linkrply"></a>';
-                        echo '<div class="rply">
-                                <img src="Image/rplyicon.svg" alt="rply" height="20" width="20">
+                        $rplyCount = $rplya['rplyCount'];?>
+                        <div class="chat-comment">
+                            <div class="account">
+                                <div class="account-image"><?php
+                                if (!empty($row['profileIcon'])) {
+                                    echo '<a href="../Profile/Profile.php"><img src="'. htmlspecialchars($row['profileIcon']) . '" alt="ProfileImage"></a>';
+                                } else {
+                                    echo '<a href="../Profile/Profile.php"><img src="../image/DefaultIcon.svg" alt="ProfileImage"></a>';
+                                }?>
+                            </div>
+                        <a href="../Profile/Profile.php?id=<?= $row['userID'] ?>"><p class="account-name"><?= htmlspecialchars($row['nickname']) ?> </p></a>
+                        </div>
+                        <a href="Globalrply.php?commentID=<?= $row['commentID'] ?>" class="linkrply">
+                                <p class="comment"><?= htmlspecialchars($row['commentText']) ?></p></a>
+                        <div class="rply">
+                                <img src="../image/RplyMark.svg" alt="rply" height="20" width="20">
                                 <div class="balloon3-left">
-                                    <p>', $rplyCount, '</p>
+                                    <p><?= $rplyCount ?></p>
                                 </div>
-                                <img src="Image/goodicon.svg" alt="good" height="20" width="20">
-                            </div>';
-                        echo '</div>';
+                                <img src="../image/GoodSine.svg" alt="good" height="20" width="20">
+                            </div>
+                        </div><?php
                     }
                 }
                 ?>
             </div>
             <!-- 入力フォーム -->
-            <?php
-            if (isset($_SESSION['users'])) {
-                $userID = $_SESSION['users']['userID'];
-            } else {
-                $userID = 0; // 例: ログインしていないユーザーのためのデフォルト値
-            }
-            ?>
             <div class="send">
                 <form action="TopPage.php" method="post" enctype="multipart/form-data" class="text-box">
-                    <input type="hidden" name="userID" value="<?php echo $userID; ?>">
-                    <input type="text" autocomplete="off" class="chat-text" placeholder="テキストを入力" name="HeaderSearch" spellcheck="false">
-                    <button type="submit" class="send-button">
-                        <img src="Image/send-icon.svg" width="20" height="20" alt="送信">
-                    </button>
+                    <input type="hidden" name="userID" value="">
+                    <input type="text" class="chat-text" placeholder="テキストを入力" name="commentText" spellcheck="false">
                     <label for="file-upload" class="send-file">
-                        <img src="Image/file-icon.svg" width="20" height="20" alt="ファイル添付">
-
+                        <img src="../image/FileIcon.svg" width="20" height="20" alt="ファイル添付">
                     </label>
                     <input type="file" id="file-upload" name="file" style="display: none;" onchange="displayFileName(this)">
+                    <button type="submit" class="send-button">
+                        <img src="../image/SendIcon.svg" width="20" height="20" alt="送信">
+                    </button>
                 </form>
             </div>
             <!-- ------------ -->
