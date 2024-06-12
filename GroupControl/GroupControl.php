43060bf0c91b1fa8a84c6a4218e87992c568e83e
somatakano <148741500+somatakano@users.noreply.github.com>
2024-06-12 11:52:39 +0900

    DBに挿入ができた

---
+++ GroupControl/GroupControl.php
@@ -0,0 +1,23 @@
+<?php session_start(); ?>
+ 
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>グループ管理画面</title>
+    <link rel="stylesheet" type="text/css" href="css/GroupCre.css">
+</head>
+ 
+<body>
+    <header>
+        <include src="../Header/Header.php"></include>
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+         
+    </footer>
+</body>
+</html>
