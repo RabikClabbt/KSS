4acda4da579beb885c601b2269aaa98fa5a7ddf2
daichi0921 <146160249+daichan115115@users.noreply.github.com>
2024-06-12 09:48:35 +0900

    Add files via upload

--- src/toroku-input.php
+++ src/toroku-input.php
@@ -1,43 +1,44 @@
 <?php session_start(); ?>
-
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>新規会員登録</title>
+    <link rel="stylesheet" href="css/Toroku.css">
 </head>
-
 <body>
     <h1>アカウント作成</h1>
-    <?php
-    $userID=$mailaddress=$password='';
-    if(isset($_SESSION['users'])){
-        $userID=$_SESSION['users']['userID'];
-        $mailaddress=$_SESSION['users']['mailaddress'];
-        $password=$_SESSION['users']['password'];
-    }
-    ?>
-    <?php
-       echo '<form action="toroku-output.php" method="post">';
-    echo '<p>';
-    echo '<h4><label for="signin-ID">ユーザーID</label></h4>';
-    echo '<input id="signin-id" type="text" required name="userID" value="',$userID,'">';
-    echo '</p>';
-    echo '<p>';
-    echo '<h4><label for="signin-pass">メールアドレス</label></h4>';
-    echo '<input id="signin-pass" type="text" required name="mailaddress" value="',$mailaddress,'">';
-    echo '</p>';
-    echo '<p>';
-    echo '<h4><label for="signin-name">パスワード</label></h4>';
-    echo '<input type="text" name="password" required name="password" value="',$password,'">';
-    echo '<a href="login.php">アカウント作成済み</a>';
-    echo '</p>';
-    echo '<br>';
-    echo ' <p><button name="touroku-button"type="submit">作成</button></p>';
-    ?>
+    <div class="form-container">
+        <?php
+        if (isset($_SESSION['error_message'])) {
+            echo '<div class="error-message">' . htmlspecialchars($_SESSION['error_message'], ENT_QUOTES, 'UTF-8') . '</div>';
+            unset($_SESSION['error_message']);
+        }
+        if (isset($_SESSION['success_message'])) {
+            echo '<div class="success-message">' . htmlspecialchars($_SESSION['success_message'], ENT_QUOTES, 'UTF-8') . '</div>';
+            unset($_SESSION['success_message']);
+        }
+        ?>
+        <form action="toroku-output.php" method="post">
+            <div class="form-group">
+                <h4><label for="signin-id">ユーザーID</label></h4>
+                <input id="signin-id" type="text" required name="userID" value="<?php echo isset($_SESSION['users']) ? htmlspecialchars($_SESSION['users']['userID'], ENT_QUOTES, 'UTF-8') : ''; ?>">
+            </div>
+            <div class="form-group">
+                <h4><label for="signin-mail">メールアドレス</label></h4>
+                <input id="signin-mail" type="email" required name="mailaddress" value="<?php echo isset($_SESSION['users']) ? htmlspecialchars($_SESSION['users']['mailaddress'], ENT_QUOTES, 'UTF-8') : ''; ?>">
+            </div>
+            <div class="form-group">
+                <h4><label for="signin-pass">パスワード</label></h4>
+                <input id="signin-pass" type="password" required name="password">
+                <a href="login.php">アカウント作成済み</a>
+            </div>
+            <div class="form-group">
+                <button type="submit">作成</button>
+            </div>
+        </form>
+    </div>
 </body>
-
-</html
+</html>
