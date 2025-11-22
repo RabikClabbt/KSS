c81313ac049fb082ce3e760f9ca9e09f97ec7fbf
RabikClabbt <ytwk62563@gmail.com>
2025-11-22 15:15:29 +0900

    commit: Config files updated

--- src/db-connect.php
+++ src/db-connect.php
@@ -1,9 +1,9 @@
 <?php
 
-    const server = "mysql305.phy.lolipop.lan"; //サーバー名
-    const dbname = "LAA1517467-yadix"; //データベース名
-    const user = "LAA1517467"; //ユーザー名
-    const pass = "YadiXadmin"; //パスワード
+    const server = "localhost"; //サーバー名
+    const dbname = "kss_db"; //データベース名
+    const user = "root"; //ユーザー名
+    const pass = ""; //パスワード
 
     $connect = 'mysql:host='. server . ';dbname='. dbname . ';charset=utf8';
     
